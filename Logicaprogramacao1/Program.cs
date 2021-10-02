4784ec6d2a506e84ac52ebbbee6a1dd95ddb3837
Aldo Boschetti Filho <abf214@gmail.com>
2021-10-02 17:16:26 -0300

    first commit

---
+++ Logicaprogramacao1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Logicaprogramacao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string sobremesa = "Pudim";
+            //bool estouAcimaDoPeso = false;
+
+            //if (estouAcimaDoPeso || sobremesa == "Pudim")
+            //{
+            //    Console.WriteLine("Comer sobremesa");
+            //}
+
+            //bool termineiTrabalho = false;
+            //bool estouComFome = true;
+
+            //if (!termineiTrabalho && estouComFome)
+            //{
+            //    Console.WriteLine("Almoçar");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Continuar Trabalhando");
+            //}
+            //int numero = 10;
+            //bool boolean = true;
+
+            //if (numero != 10 && boolean == true)
+            //{
+                
+            //    Console.WriteLine("A condição é verdadeira!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("A condição é falsa!");
+            //}
+            
+            
+            
+            //string nome = "Aldo";
+            //string idade = "65";
+            //string bemVindo = $"Seja bem vindo, {nome}. Sua Idade é de: {idade} anos";
+            //Console.WriteLine(bemVindo);
+            
+            //Expressões lógicas e CONSTANTES
+            //const int constInteiro = 20;
+            //int inteiro1 = 10;
+            //int inteiro2 = 20;
+            //int soma = inteiro1 + inteiro2;
+            //Console.WriteLine(soma);
+            //inteiro1 = 50 + soma;
+            //Console.WriteLine(inteiro1 * 2);
+
+
+
+            //Aprendendo a declarar variáveis
+            //string hello = "Seja bem vindo a nossa aula!";
+            //int inteiro = 1;
+            //double numero = 1.5;
+            //bool booleano = false;
+
+            //Aprendendo sobre Arrays
+            //int[] numeros = new int[5];
+            //numeros[0] = 10;
+            //numeros[1] = 20;
+            //numeros[2] = 50;
+            //numeros[3] = 100;
+            //numeros[4] = 200;
+        }
+    }
+}
